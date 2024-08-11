0183fc55fc5a5e1a6241885c9330bede9fd3290c
philopence <philopence@gmail.com>
2024-08-11 13:44:41 +0800

    feat(ui): built sidebar

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,56 @@
+import {
+  House,
+  LayoutDashboard,
+  ScrollText,
+  Settings,
+  Users,
+} from "lucide-react";
+import { NavLink } from "react-router-dom";
+
+const navLinks = [
+  {
+    label: "Dashboard",
+    to: "/dashboard",
+    icon: <LayoutDashboard />,
+  },
+  {
+    label: "Bookings",
+    to: "/bookings",
+    icon: <ScrollText />,
+  },
+  {
+    label: "Cabins",
+    to: "/cabins",
+    icon: <House />,
+  },
+  {
+    label: "Users",
+    to: "/users",
+    icon: <Users />,
+  },
+  {
+    label: "Settings",
+    to: "/Settings",
+    icon: <Settings />,
+  },
+];
+
+export default function SideBar() {
+  return (
+    <aside className="space-y-8 p-8">
+      <img src="/react.svg" alt="logo" />
+      <nav>
+        <ul>
+          {navLinks.map((link) => (
+            <li key={link.label}>
+              <NavLink className="flex items-center" to={link.to}>
+                {link.icon}
+                <span>{link.label}</span>
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </aside>
+  );
+}
--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,9 +1,12 @@
+import SideBar from "@/components/SideBar";
 import { Outlet } from "react-router-dom";
 
 export default function AppLayout() {
   return (
     <div className="grid min-h-dvh grid-cols-[auto_1fr]">
-      <div>sidebar</div>
+      <div>
+        <SideBar />
+      </div>
       <div className="grid grid-rows-[auto_1fr]">
         <div>header</div>
         <div>
