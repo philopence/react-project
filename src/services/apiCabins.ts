cbbd59b4df96102e4be27ad41a09eab6bd06937c
philopence <philopence@gmail.com>
2024-08-13 07:46:42 +0800

    refactor: get cabins

--- src/services/apiCabins.ts
+++ src/services/apiCabins.ts
@@ -1,13 +1,19 @@
 import { faker } from "@faker-js/faker";
 
+const cabins = Array.from({ length: 5 }).map((_, i) => ({
+  id: i + 1,
+  image: faker.image.url({ width: 300, height: 200 }),
+  name: faker.commerce.productName(),
+  price: faker.number.int({ min: 100, max: 1000 }),
+  discount: faker.number.int({ min: 1, max: 99 }),
+  description: faker.commerce.productDescription(),
+  maxCapacity: faker.number.int({ min: 1, max: 8 }),
+}));
+
 export async function getCabins() {
-  return Array.from({ length: 5 }).map((_, i) => ({
-    id: i + 1,
-    image: faker.image.url({ width: 300, height: 200 }),
-    name: faker.commerce.productName(),
-    price: faker.number.int({ min: 100, max: 1000 }),
-    discount: faker.number.int({ min: 1, max: 99 }),
-    description: faker.commerce.productDescription(),
-    maxCapacity: faker.number.int({ min: 1, max: 8 }),
-  }));
+  return cabins;
+}
+
+export async function deleteCabinById(id: number) {
+  console.log(id);
 }
