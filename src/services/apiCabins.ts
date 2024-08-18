42fb6c2d8b8e4294adc3d5ec50b6567adf39b39d
philopence <philopence@gmail.com>
2024-08-18 16:01:39 +0800

    refactor: cabin api

--- src/services/apiCabins.ts
+++ src/services/apiCabins.ts
@@ -1,19 +1,67 @@
-import { faker } from "@faker-js/faker";
+import { cabinApiSchema, CabinForm } from "@/schemas/cabin";
 
-const cabins = Array.from({ length: 5 }).map((_, i) => ({
-  id: i + 1,
-  image: faker.image.url({ width: 300, height: 200 }),
-  name: faker.commerce.productName(),
-  price: faker.number.int({ min: 100, max: 1000 }),
-  discount: faker.number.int({ min: 1, max: 99 }),
-  description: faker.commerce.productDescription(),
-  maxCapacity: faker.number.int({ min: 1, max: 8 }),
-}));
+export async function createCabin(cabin: CabinForm) {
+  const { image, ...otherFields } = cabin;
+  const formData = new FormData();
+  formData.append("image", image);
+  formData.append("otherFields", JSON.stringify(otherFields));
+
+  const res = await fetch("/api/cabins", {
+    method: "POST",
+    body: formData,
+  });
+
+  if (!res.ok) throw new Error(res.statusText);
+}
 
 export async function getCabins() {
+  const res = await fetch("/api/cabins", {
+    method: "GET",
+  });
+
+  if (!res.ok) throw new Error(res.statusText);
+
+  const cabins = await cabinApiSchema.array().parseAsync(await res.json());
+
   return cabins;
 }
 
-export async function deleteCabinById(id: number) {
-  console.log(id);
+export async function getCabinById(id: string) {
+  const res = await fetch(`/api/cabins/${id}`, {
+    method: "GET",
+  });
+
+  if (!res.ok) throw new Error(res.statusText);
+
+  const cabin = cabinApiSchema.parse(await res.json());
+
+  return cabin;
+}
+
+export async function updateCabinById({
+  id,
+  cabin,
+}: {
+  id: string;
+  cabin: CabinForm;
+}) {
+  const { image, ...otherFields } = cabin;
+  const formData = new FormData();
+  formData.append("image", image);
+  formData.append("otherFields", JSON.stringify(otherFields));
+
+  const res = await fetch(`/api/cabins/${id}`, {
+    method: "PATCH",
+    body: formData,
+  });
+
+  if (!res.ok) throw new Error(res.statusText);
+}
+
+export async function deleteCabinById(id: string) {
+  const res = await fetch(`/api/cabins/${id}`, {
+    method: "DELETE",
+  });
+
+  if (!res.ok) throw new Error(res.statusText);
 }
