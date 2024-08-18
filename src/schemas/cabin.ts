40e2653895c9e3af528ff7e5d193f92303cf86c7
philopence <philopence@gmail.com>
2024-08-18 16:01:12 +0800

    feat: add zod schemas to validate api and form

---
+++ src/schemas/cabin.ts
@@ -0,0 +1,21 @@
+import { z } from "zod";
+
+const cabinSchema = z.object({
+  name: z.string(),
+  description: z.string(),
+  maxCapacity: z.coerce.number(),
+  price: z.coerce.number(),
+  discount: z.coerce.number(),
+});
+
+export const cabinFormSchema = cabinSchema.extend({
+  image: z.instanceof(File).refine((data) => data.type.startsWith("image/")),
+});
+
+export const cabinApiSchema = cabinSchema.extend({
+  _id: z.string(),
+  image: z.string(),
+});
+
+export type CabinForm = z.infer<typeof cabinFormSchema>;
+export type CabinApi = z.infer<typeof cabinApiSchema>;
