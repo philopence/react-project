7eb14befef5eec676b74f060ab34bbd77065f360
philopence <philopence@gmail.com>
2024-08-28 21:48:04 +0800

    feat: get bookings with query

--- src/features/booking/useGetBookingsQuery.ts
+++ src/features/booking/useGetBookingsQuery.ts
@@ -1,10 +1,24 @@
 import { useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "react-router-dom";
 import { getBookings } from "@/services/apiBooking";
 
 export default function useGetBookingsQuery() {
+  const [searchParams] = useSearchParams();
+
+  // const queries = Object.fromEntries(searchParams.entries());
+
+  // Support fields: status
+  const filterValue = searchParams.get("status");
+
+  // No filtering when status is equal to "all"
+  const filterQuery =
+    filterValue && filterValue !== "all"
+      ? new URLSearchParams({ status: filterValue }).toString()
+      : "";
+
   const query = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getBookings,
+    queryKey: ["bookings", filterQuery],
+    queryFn: () => getBookings(filterQuery),
   });
 
   return query;
