d037997081b682309dd5d9e08a4f2e1f27943cd7
philopence <philopence@gmail.com>
2024-08-28 21:49:17 +0800

    feat: update BookingsPage

--- src/pages/BookingsPage.tsx
+++ src/pages/BookingsPage.tsx
@@ -1,9 +1,23 @@
+import BookingFilter from "@/features/booking/BookingFilter";
+import BookingSorter from "@/features/booking/BookingSorter";
 import BookingsTable from "@/features/booking/BookingsTable";
 
 export default function BookingsPage() {
   return (
     <div>
-      <header>Booking Page Header</header>
+      <header>
+        <div>Booking Page Header</div>
+
+        <div className="flex items-center gap-2">
+          <div>
+            <BookingFilter />
+          </div>
+
+          <div>
+            <BookingSorter />
+          </div>
+        </div>
+      </header>
       <main>
         <BookingsTable />
       </main>
