4fbdb27656963d2090a07bafe479514fec427fd0
philopence <philopence@gmail.com>
2024-08-28 21:47:38 +0800

    refactor: BookingsTable

--- src/features/booking/BookingsTable.tsx
+++ src/features/booking/BookingsTable.tsx
@@ -1,4 +1,7 @@
+import "date-fns";
+import { formatDistance } from "date-fns";
 import { EllipsisVertical } from "lucide-react";
+import { Badge } from "@/components/ui/badge";
 import {
   Table,
   TableBody,
@@ -9,6 +12,7 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import useGetBookingsQuery from "@/features/booking/useGetBookingsQuery";
+import { formatCurrency, formatDateTime } from "@/lib/utils";
 import { Booking } from "@/schemas/booking";
 
 export default function BookingsTable() {
@@ -43,16 +47,49 @@ export default function BookingsTable() {
 }
 
 function BookingRow({ booking }: { booking: Booking }) {
+  const statusStyles = {
+    unconfirmed: "bg-yellow-500",
+    "check-in": "bg-blue-500",
+    "check-out": "bg-slate-500",
+  };
   return (
     <TableRow key={booking._id}>
-      <TableCell>{booking.cabin}</TableCell>
-      <TableCell>{booking.guest}</TableCell>
+      <TableCell>{booking.cabin.name}</TableCell>
       <TableCell>
-        {booking.startDate}-{booking.endDate}
+        <div className="flex flex-col gap-1">
+          <span className="font-medium">{booking.guest.name}</span>
+          <span className="text-sm text-secondary-foreground">
+            {booking.guest.email}
+          </span>
+        </div>
       </TableCell>
-      <TableCell>{booking.status}</TableCell>
-      <TableCell>{booking.guestNum}</TableCell>
       <TableCell>
+        <div className="flex flex-col gap-1">
+          <span className="font-medium">
+            {formatDistance(new Date(booking.startDate), new Date(), {
+              addSuffix: true,
+            })}
+            -
+            {formatDistance(
+              new Date(booking.endDate),
+              new Date(booking.startDate),
+              { addSuffix: false },
+            )}
+          </span>
+          <span className="text-sm text-secondary-foreground">
+            {formatDateTime(booking.startDate)} -{" "}
+            {formatDateTime(booking.endDate)}
+          </span>
+        </div>
+      </TableCell>
+      <TableCell>
+        <Badge className={statusStyles[booking.status]}>{booking.status}</Badge>
+      </TableCell>
+      <TableCell>
+        {formatCurrency(booking.cabinPrice + booking.extraPrice)}
+      </TableCell>
+      <TableCell>
+        {/* TODO the dropdownmenu of actions */}
         <EllipsisVertical />
       </TableCell>
     </TableRow>
