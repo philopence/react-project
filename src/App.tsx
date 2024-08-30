003c9f560ce0ded16a5654529c7d62998f21f9aa
philopence <philopence@gmail.com>
2024-08-30 22:23:56 +0800

    feat: add ThemeProvider to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import Query from "@/Query";
 import Router from "@/Router";
 import { Toaster } from "@/components/ui/toaster";
+import { ThemeProvider } from "./contexts/theme";
 
 export default function App() {
   return (
-    <Query>
-      <Router />
-      <Toaster />
-    </Query>
+    <ThemeProvider>
+      <Query>
+        <Router />
+        <Toaster />
+      </Query>
+    </ThemeProvider>
   );
 }
