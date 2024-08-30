a67d0b125211f87cdba22b9107aad49b6ff98637
philopence <philopence@gmail.com>
2024-08-30 23:37:33 +0800

    feat: add getBookingById service

--- src/services/apiBooking.ts
+++ src/services/apiBooking.ts
@@ -1,5 +1,6 @@
 import { ApiError } from "@/lib/ApiError";
-import { Bookings, bookingsSchema } from "@/schemas/booking";
+import { Bookings, bookingSchema, bookingsSchema } from "@/schemas/booking";
+import { resErrorSchema } from "@/schemas/error";
 
 // ?field[gt]=5&page=2&limit=3
 export async function getBookings(query?: string): Promise<Bookings> {
@@ -17,6 +18,30 @@ export async function getBookings(query?: string): Promise<Bookings> {
     const data = bookingsSchema.parse(rawData);
 
     console.log(data.totalBooking);
+
+    return data;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+}
+
+/**
+ * GET {{baseURL}}/bookings/66cd68a5f7f7fc968f4c4dcc
+ */
+export async function getBookingById(id: string) {
+  try {
+    const res = await fetch(`/api/v1/bookings/${id}`, {
+      method: "GET",
+    });
+
+    if (!res.ok) {
+      const data = resErrorSchema.parse(await res.json());
+
+      throw new Error(data.message);
+    }
+
+    const data = bookingSchema.parse(await res.json());
 
     return data;
   } catch (err) {
