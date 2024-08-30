c0ae2f7db418937009ebd55a2b998d15a9625cd0
philopence <philopence@gmail.com>
2024-08-30 22:23:37 +0800

    refactor: api booking

--- src/services/apiBooking.ts
+++ src/services/apiBooking.ts
@@ -1,15 +1,22 @@
 import { ApiError } from "@/lib/ApiError";
-import { Booking, bookingSchema } from "@/schemas/booking";
+import { Bookings, bookingsSchema } from "@/schemas/booking";
 
-export async function getBookings(query: string = ""): Promise<Booking[]> {
+// ?field[gt]=5&page=2&limit=3
+export async function getBookings(query?: string): Promise<Bookings> {
   try {
-    const res = await fetch(`/api/v1/bookings?${query}`, { method: "GET" });
+    const queryString = query ? `?${query}` : "";
+
+    const res = await fetch(`/api/v1/bookings/${queryString}`, {
+      method: "GET",
+    });
 
     if (!res.ok) throw new ApiError(res.status, (await res.json()).message);
 
     const rawData = await res.json();
 
-    const data = bookingSchema.array().parse(rawData);
+    const data = bookingsSchema.parse(rawData);
+
+    console.log(data.totalBooking);
 
     return data;
   } catch (err) {
