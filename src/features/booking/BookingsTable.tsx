d901188ef0ae0d43e4db500d99e91c71ad26b744
philopence <philopence@gmail.com>
2024-09-01 23:27:08 +0800

    feat: check out

--- src/features/booking/BookingsTable.tsx
+++ src/features/booking/BookingsTable.tsx
@@ -1,4 +1,5 @@
 import { DropdownMenu } from "@radix-ui/react-dropdown-menu";
+import { useQueryClient } from "@tanstack/react-query";
 import "date-fns";
 import { formatDistance } from "date-fns";
 import { EllipsisVertical } from "lucide-react";
@@ -10,7 +11,7 @@ import {
   DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuSeparator,
-  DropdownMenuTrigger,
+  DropdownMenuTrigger
 } from "@/components/ui/dropdown-menu";
 import {
   Table,
@@ -19,11 +20,13 @@ import {
   TableCell,
   TableHead,
   TableHeader,
-  TableRow,
+  TableRow
 } from "@/components/ui/table";
+import { useToast } from "@/components/ui/use-toast";
 import useGetBookingsQuery from "@/features/booking/useGetBookingsQuery";
 import { formatCurrency, formatDateTime } from "@/lib/utils";
 import { Booking } from "@/schemas/booking";
+import useUpdateBookingByIdMutation from "./useUpdateBookingByIdMutation";
 
 export default function BookingsTable() {
   const { data, isLoading } = useGetBookingsQuery();
@@ -85,10 +88,36 @@ function BookingList({ bookings = [] }: { bookings?: Booking[] }) {
 const STATUS_STYLES = {
   unconfirmed: "bg-yellow-500",
   "check-in": "bg-blue-500",
-  "check-out": "bg-slate-500",
+  "check-out": "bg-slate-500"
 };
 
 function BookingRow({ booking }: { booking: Booking }) {
+  const { isPending, mutate: checkOutMutate } = useUpdateBookingByIdMutation();
+
+  const { toast } = useToast();
+
+  const queryClient = useQueryClient();
+
+  function handleCheckOut() {
+    checkOutMutate(
+      {
+        id: booking._id,
+        booking: {
+          status: "check-out"
+        }
+      },
+      {
+        onSuccess: () => {
+          queryClient.invalidateQueries({
+            queryKey: ["bookings", booking._id]
+          });
+
+          toast({ description: "check out successfully" });
+        }
+      }
+    );
+  }
+
   return (
     <TableRow key={booking._id}>
       <TableCell>{booking.cabin.name}</TableCell>
@@ -104,13 +133,13 @@ function BookingRow({ booking }: { booking: Booking }) {
         <div className="flex flex-col gap-1">
           <span className="font-medium">
             {formatDistance(new Date(booking.startDate), new Date(), {
-              addSuffix: true,
+              addSuffix: true
             })}
             -
             {formatDistance(
               new Date(booking.endDate),
               new Date(booking.startDate),
-              { addSuffix: false },
+              { addSuffix: false }
             )}
           </span>
           <span className="text-sm text-secondary-foreground">
@@ -141,6 +170,18 @@ function BookingRow({ booking }: { booking: Booking }) {
                 Booking Detail
               </Link>
             </DropdownMenuItem>
+            {booking.status === "unconfirmed" && (
+              <DropdownMenuItem asChild>
+                <Link to={{ pathname: `/bookings/${booking._id}/check-in` }}>
+                  Check In
+                </Link>
+              </DropdownMenuItem>
+            )}
+            {booking.status === "check-in" && (
+              <DropdownMenuItem disabled={isPending} onClick={handleCheckOut}>
+                Check out
+              </DropdownMenuItem>
+            )}
           </DropdownMenuContent>
         </DropdownMenu>
       </TableCell>
