0fa6174ade33ec5047c70d5c6ba6e03a752e202d
philopence <philopence@gmail.com>
2024-09-02 08:53:43 +0800

    feat(utils): add wait function to mock delay

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,17 +5,27 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+/**
+ * @description wait timeout ms
+ * @param {number} timeout ms
+ */
+export async function wait(timeout: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, timeout);
+  });
+}
+
 export function formatCurrency(num: number): string {
   return new Intl.NumberFormat("en-US", {
     style: "currency",
-    currency: "USD",
+    currency: "USD"
   }).format(num);
 }
 
 export function formatDateTime(date: string): string {
   return new Intl.DateTimeFormat("en-US", {
     dateStyle: "medium",
-    timeStyle: "short",
+    timeStyle: "short"
     // timeZone: "Asia/Shanghai",
   }).format(new Date(date));
 }
