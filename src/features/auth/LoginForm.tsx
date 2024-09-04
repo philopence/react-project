c7779585661fdbbe756e4e5c2a859d7fb66df299
philopence <philopence@gmail.com>
2024-09-04 13:06:21 +0800

    feat: create LoginForm

---
+++ src/features/auth/LoginForm.tsx
@@ -0,0 +1,61 @@
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import useLoginForm, { LoginFormValues } from "./useLoginForm";
+import useLoginMutation from "./useLoginMutation";
+
+export default function LoginForm() {
+  const form = useLoginForm();
+  const loginMutation = useLoginMutation();
+
+  function onSubmit(values: LoginFormValues) {
+    console.log(values);
+    loginMutation.mutate(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input
+                  type="email"
+                  placeholder="example@email.com"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input type="password" placeholder="********" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Login</Button>
+      </form>
+    </Form>
+  );
+}
