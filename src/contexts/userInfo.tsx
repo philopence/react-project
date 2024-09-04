b9282109c06a17d455b92f5cbb5b58cf709229ea
philopence <philopence@gmail.com>
2024-09-04 20:14:20 +0800

    feat: create userInfo context

--- src/contexts/userInfo.tsx
+++ src/contexts/userInfo.tsx
@@ -1,10 +1,4 @@
-import {
-  createContext,
-  PropsWithChildren,
-  useContext,
-  useEffect,
-  useState
-} from "react";
+import { createContext, PropsWithChildren, useContext, useState } from "react";
 
 type UserInfo = {
   _id: string;
@@ -21,23 +15,8 @@ const UserInfoProviderContext = createContext<UserInfoProviderState | null>(
   null
 );
 
-export default function UserInfoProvider({
-  storageKey = "userInfo",
-  children
-}: PropsWithChildren<{
-  storageKey?: string;
-}>) {
-  const [userInfo, setUserInfo] = useState<UserInfo | null>(() => {
-    const userInfo = localStorage.getItem(storageKey);
-
-    if (userInfo === null) return null;
-
-    return JSON.parse(userInfo);
-  });
-
-  useEffect(() => {
-    localStorage.setItem(storageKey, JSON.stringify(userInfo));
-  }, [userInfo, storageKey]);
+export default function UserInfoProvider({ children }: PropsWithChildren) {
+  const [userInfo, setUserInfo] = useState<UserInfo | null>(null);
 
   function storageUserInfo(userInfo: UserInfo) {
     setUserInfo(userInfo);
