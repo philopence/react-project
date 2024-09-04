d1c6fbc7099103e70b9574ce54e0766fc2995532
philopence <philopence@gmail.com>
2024-09-04 20:30:54 +0800

    feat: toast message when access failed

--- src/components/ProtectedRoutes.tsx
+++ src/components/ProtectedRoutes.tsx
@@ -2,30 +2,39 @@ import { useEffect } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import { useUserInfoContext } from "@/contexts/userInfo";
 import useGetUserInfoQuery from "@/features/auth/useGetUserInfoQuery";
+import { useToast } from "./ui/use-toast";
 
 export default function ProtectedRoutes() {
   const getUserInfoQuery = useGetUserInfoQuery();
 
   const userInfoContext = useUserInfoContext();
 
+  const { toast } = useToast();
+
   useEffect(() => {
     if (!getUserInfoQuery.isPending && !getUserInfoQuery.isError) {
       userInfoContext.storageUserInfo(getUserInfoQuery.data);
+    }
+
+    if (getUserInfoQuery.isError) {
+      toast({
+        variant: "destructive",
+        description: getUserInfoQuery.error.message
+      });
     }
   }, [
     getUserInfoQuery.data,
     getUserInfoQuery.isError,
     getUserInfoQuery.isPending,
-    userInfoContext
+    userInfoContext,
+    toast,
+    getUserInfoQuery
   ]);
 
   if (getUserInfoQuery.isPending) return "Loading...";
 
-  if (getUserInfoQuery.isError) {
-    console.log(getUserInfoQuery.error.message);
-
+  if (getUserInfoQuery.isError)
     return <Navigate to={{ pathname: "/login" }} replace />;
-  }
 
   // userInfoContext.storageUserInfo(getUserInfoQuery.data);
 
