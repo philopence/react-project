0cef71b1784567c9bcdc61551c4cf7f1ace34df9
philopence <philopence@gmail.com>
2024-09-04 20:30:05 +0800

    refactor: routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,76 +3,82 @@ import {
   Navigate,
   RouterProvider
 } from "react-router-dom";
+import ProtectedRoutes from "@/components/ProtectedRoutes";
 import AppLayout from "@/layouts/AppLayout";
+import BookingPage from "@/pages/BookingPage";
 import BookingsPage from "@/pages/BookingsPage";
 import CabinsPage from "@/pages/CabinsPage";
+import CheckPage from "@/pages/CheckPage";
+import CreateCabinPage from "@/pages/CreateCabinPage";
 import DashboardPage from "@/pages/DashboardPage";
+import LoginPage from "@/pages/LoginPage";
 import SettingsPage from "@/pages/SettingsPage";
+import UpdateCabinPage from "@/pages/UpdateCabinPage";
 import UsersPage from "@/pages/UsersPage";
-import BookingPage from "./pages/BookingPage";
-import CheckPage from "./pages/CheckPage";
-import CreateCabinPage from "./pages/CreateCabinPage";
-import LoginPage from "./pages/LoginPage";
-import UpdateCabinPage from "./pages/UpdateCabinPage";
 
 const router = createBrowserRouter([
   {
-    path: "login",
+    path: "/login",
     element: <LoginPage />
   },
   {
-    path: "/",
-    element: <AppLayout />,
+    element: <ProtectedRoutes />,
     children: [
       {
-        index: true,
-        element: <Navigate replace to="/dashboard" />
-      },
-      {
-        path: "dashboard",
-        element: <DashboardPage />
-      },
-      {
-        path: "bookings",
+        path: "/",
+        element: <AppLayout />,
         children: [
           {
             index: true,
-            element: <BookingsPage />
+            element: <Navigate replace to="/dashboard" />
           },
           {
-            path: ":id",
-            element: <BookingPage />
+            path: "dashboard",
+            element: <DashboardPage />
           },
           {
-            path: ":id/check",
-            element: <CheckPage />
+            path: "bookings",
+            children: [
+              {
+                index: true,
+                element: <BookingsPage />
+              },
+              {
+                path: ":id",
+                element: <BookingPage />
+              },
+              {
+                path: ":id/check",
+                element: <CheckPage />
+              }
+            ]
+          },
+          {
+            path: "cabins",
+            children: [
+              {
+                index: true,
+                element: <CabinsPage />
+              },
+              {
+                path: "new",
+                element: <CreateCabinPage />
+              },
+              {
+                path: "edit/:id",
+                element: <UpdateCabinPage />
+              }
+            ]
+          },
+          {
+            path: "users",
+            element: <UsersPage />
+          },
+          {
+            path: "settings",
+            element: <SettingsPage />
           }
         ]
-      },
-      {
-        path: "cabins",
-        children: [
-          {
-            index: true,
-            element: <CabinsPage />
-          },
-          {
-            path: "new",
-            element: <CreateCabinPage />
-          },
-          {
-            path: "edit/:id",
-            element: <UpdateCabinPage />
-          }
-        ]
-      },
-      {
-        path: "users",
-        element: <UsersPage />
-      },
-      {
-        path: "settings",
-        element: <SettingsPage />
       }
     ]
   }
