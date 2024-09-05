f85ef3931038b30d002f8a17199948148927f8fc
philopence <philopence@gmail.com>
2024-09-05 08:45:45 +0800

    fix: don't need userInfo context (#6)

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,14 @@ import Query from "@/Query";
 import Router from "@/Router";
 import { Toaster } from "@/components/ui/toaster";
 import { ThemeProvider } from "./contexts/theme";
-import UserInfoProvider from "./contexts/userInfo";
 
 export default function App() {
   return (
     <ThemeProvider>
-      <UserInfoProvider>
-        <Query>
-          <Router />
-          <Toaster />
-        </Query>
-      </UserInfoProvider>
+      <Query>
+        <Router />
+        <Toaster />
+      </Query>
     </ThemeProvider>
   );
 }
--- src/components/ProtectedRoutes.tsx
+++ src/components/ProtectedRoutes.tsx
@@ -1,42 +1,26 @@
 import { useEffect } from "react";
 import { Navigate, Outlet } from "react-router-dom";
-import { useUserInfoContext } from "@/contexts/userInfo";
 import useGetUserInfoQuery from "@/features/auth/useGetUserInfoQuery";
 import { useToast } from "./ui/use-toast";
 
 export default function ProtectedRoutes() {
   const getUserInfoQuery = useGetUserInfoQuery();
 
-  const userInfoContext = useUserInfoContext();
-
   const { toast } = useToast();
 
   useEffect(() => {
-    if (!getUserInfoQuery.isPending && !getUserInfoQuery.isError) {
-      userInfoContext.storageUserInfo(getUserInfoQuery.data);
-    }
-
     if (getUserInfoQuery.isError) {
       toast({
         variant: "destructive",
         description: getUserInfoQuery.error.message
       });
     }
-  }, [
-    getUserInfoQuery.data,
-    getUserInfoQuery.isError,
-    getUserInfoQuery.isPending,
-    userInfoContext,
-    toast,
-    getUserInfoQuery
-  ]);
+  }, [getUserInfoQuery.error?.message, getUserInfoQuery.isError, toast]);
 
   if (getUserInfoQuery.isPending) return "Loading...";
 
   if (getUserInfoQuery.isError)
     return <Navigate to={{ pathname: "/login" }} replace />;
-
-  // userInfoContext.storageUserInfo(getUserInfoQuery.data);
 
   return <Outlet />;
 }
