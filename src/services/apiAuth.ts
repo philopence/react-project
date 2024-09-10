4aae80092f0e045f5ca76b1f06e3f65a6c451e95
philopence <philopence@gmail.com>
2024-09-10 23:32:46 +0800

    feat: register logic

--- src/services/apiAuth.ts
+++ src/services/apiAuth.ts
@@ -1,19 +1,35 @@
-export async function register({
-  name,
-  email,
-  password
-}: {
+import { z } from "zod";
+
+export async function register(userData: {
   name: string;
   email: string;
   password: string;
+  image?: string;
 }) {
-  console.log(name, email, password);
+  const res = await fetch("/api/v1/auth/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(userData)
+  });
 
-  return {
-    _id: String(new Date().getTime()),
-    name,
-    email
-  };
+  if (!res.ok) throw new Error();
+
+  const rawData = await res.json();
+
+  const result = z
+    .object({
+      _id: z.string(),
+      email: z.string().email(),
+      name: z.string(),
+      image: z.string().url().nullable()
+    })
+    .safeParse(rawData);
+
+  if (!result.success) throw new Error();
+
+  return result.data;
 }
 
 export async function login({
