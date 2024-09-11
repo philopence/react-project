d0f60ff9a9d233d9f59422566a36e90eb338d408
philopence <philopence@gmail.com>
2024-09-11 09:13:25 +0800

    fix: toggle theme

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -5,23 +5,25 @@ import { Button } from "./ui/button";
 export default function ThemeToggle() {
   const { setTheme } = useThemeContext();
 
-  function handleThemeToggle(e: React.MouseEvent) {
-    const el = e.target as SVGElement;
-    if (el.id === "moon") setTheme("dark");
-    if (el.id === "sun") setTheme("light");
-  }
-
   return (
-    <Button variant="outline" size="icon" onClick={handleThemeToggle}>
-      <Moon
-        id="moon"
-        className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-      />
-      <Sun
-        id="sun"
-        className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-      />
+    <div>
+      <Button
+        className="dark:hidden"
+        variant="outline"
+        size="icon"
+        onClick={() => setTheme("dark")}
+      >
+        <Moon id="moon" />
+      </Button>
+      <Button
+        className="hidden dark:inline-flex"
+        variant="outline"
+        size="icon"
+        onClick={() => setTheme("light")}
+      >
+        <Sun id="sun" />
+      </Button>
       <span className="sr-only">Toggle theme</span>
-    </Button>
+    </div>
   );
 }
