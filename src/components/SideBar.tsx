4cd114132b9103fb05b01f2e051182e80dfe0274
philopence <philopence@gmail.com>
2024-09-22 22:06:10 +0800

    refactor: sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -5,6 +5,7 @@ import {
   Settings,
   Users
 } from "lucide-react";
+import { PropsWithChildren } from "react";
 import { NavLink } from "react-router-dom";
 
 const navLinks = [
@@ -42,15 +43,29 @@ export default function SideBar() {
       <nav>
         <ul>
           {navLinks.map((link) => (
-            <li key={link.label}>
-              <NavLink className="flex items-center" to={link.to}>
-                {link.icon}
-                <span>{link.label}</span>
-              </NavLink>
-            </li>
+            <LinkItem key={link.to} to={link.to}>
+              {link.icon}
+              <span>{link.label}</span>
+            </LinkItem>
           ))}
         </ul>
       </nav>
     </aside>
+  );
+}
+
+/**
+ * CSS: .active {} for current link
+ */
+function LinkItem({
+  to,
+  children
+}: PropsWithChildren<{
+  to: string;
+}>) {
+  return (
+    <li>
+      <NavLink to={to}>{children}</NavLink>
+    </li>
   );
 }
