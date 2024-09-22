7f9cbaf867c09d8ea5e138f8e639d2a8a9f72f76
philopence <philopence@gmail.com>
2024-09-22 22:07:16 +0800

    refactor: useCabinForm

--- src/features/cabin/useCabinForm.ts
+++ src/features/cabin/useCabinForm.ts
@@ -3,17 +3,18 @@ import { useForm } from "react-hook-form";
 import { CabinFormValues, CabinFormSchema } from "@/schemas/form";
 
 export default function useCabinForm(
-  defaultValues: Omit<CabinFormValues, "image"> = {
-    name: "",
-    description: "",
-    price: 0,
-    maxCapacity: 0,
-    discount: 0
-  }
+  defaultValues?: Omit<CabinFormValues, "image">
 ) {
+  console.log(defaultValues);
   const form = useForm<CabinFormValues>({
     resolver: zodResolver(CabinFormSchema),
-    defaultValues
+    defaultValues: defaultValues || {
+      name: "",
+      description: "",
+      price: 0,
+      maxCapacity: 0,
+      discount: 0
+    }
   });
 
   return form;
