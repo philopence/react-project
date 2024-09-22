0e3775432e83aac1d6c8762e51982d4691cfed68
philopence <philopence@gmail.com>
2024-09-22 22:07:34 +0800

    refactor: AppLayout

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,8 +1,9 @@
+import { PropsWithChildren } from "react";
 import { Link, Outlet } from "react-router-dom";
 import SideBar from "@/components/SideBar";
 import ThemeToggle from "@/components/ThemeToggle";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Button } from "@/components/ui/button";
+import { buttonVariants } from "@/components/ui/button";
 import LogoutButton from "@/features/auth/LogoutButton";
 import useGetUserInfoQuery from "@/features/auth/useGetUserInfoQuery";
 
@@ -37,17 +38,27 @@ function Header() {
 
   return (
     <header className="flex items-center justify-end gap-2">
-      {image && (
-        <Avatar>
-          <AvatarImage src={image} />
-          <AvatarFallback>{name}</AvatarFallback>
-        </Avatar>
-      )}
-      <Button variant={"link"} asChild>
-        <Link to={{ pathname: "/profile" }}>{getUserInfoQuery.data.name}</Link>
-      </Button>
+      {image && <UserAvatar name={name} image={image} />}
+      <Link
+        className={buttonVariants({ variant: "link" })}
+        to={{ pathname: "/profile" }}
+      >
+        {getUserInfoQuery.data.name}
+      </Link>
       <LogoutButton />
       <ThemeToggle />
     </header>
+  );
+}
+
+function UserAvatar({
+  name,
+  image
+}: PropsWithChildren<{ name: string; image: string }>) {
+  return (
+    <Avatar>
+      <AvatarImage src={image} alt={name} />
+      <AvatarFallback>{name}</AvatarFallback>
+    </Avatar>
   );
 }
