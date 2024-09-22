a6bf8d6e7420c74f413410de23872ad535d4e446
philopence <philopence@gmail.com>
2024-09-22 22:05:41 +0800

    feat: don't show toast when redirect to login page

--- src/components/ProtectedRoutes.tsx
+++ src/components/ProtectedRoutes.tsx
@@ -1,21 +1,8 @@
-import { useEffect } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import useGetUserInfoQuery from "@/features/auth/useGetUserInfoQuery";
-import { useToast } from "./ui/use-toast";
 
 export default function ProtectedRoutes() {
   const getUserInfoQuery = useGetUserInfoQuery();
-
-  const { toast } = useToast();
-
-  useEffect(() => {
-    if (getUserInfoQuery.isError) {
-      toast({
-        variant: "destructive",
-        description: "You need to login first"
-      });
-    }
-  }, [getUserInfoQuery.error?.message, getUserInfoQuery.isError, toast]);
 
   if (getUserInfoQuery.isPending) return "Loading...";
 
